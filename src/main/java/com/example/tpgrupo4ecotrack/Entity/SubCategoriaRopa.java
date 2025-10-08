719987b629651873a9d9d79e1b35cb6febeebded
AndreAP472005 <andu.ap111@gmail.com>
2025-10-08 14:26:58 -0500

    tercer envío Andre

--- src/main/java/com/example/tpgrupo4ecotrack/Entity/SubCategoriaRopa.java
+++ src/main/java/com/example/tpgrupo4ecotrack/Entity/SubCategoriaRopa.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.Date;
